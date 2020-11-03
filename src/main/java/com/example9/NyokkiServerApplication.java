2d5885ffd21edbf0cec7fd2225cbd3489b4465ad
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-03 20:44:18 +0900

    First Commit

---
+++ src/main/java/com/example9/NyokkiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyokkiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyokkiServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example9/User.java
@@ -0,0 +1,5 @@
+package com.example9;
+
+public class User {
+
+}
