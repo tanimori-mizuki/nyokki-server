9946efbc26ede282f2f2875bad2ec7f1fd27f008
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-05 13:42:01 +0900

    必要なクラスのインポートを行いました。

--- src/main/java/com/example9/common/EnvironmentsConfiguration.java
+++ src/main/java/com/example9/common/EnvironmentsConfiguration.java
@@ -1,5 +1,19 @@
 package com.example9.common;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+/**
+ * 環境変数(ymlに設定)を参照するクラス.
+ * 
+ * @author mizuki.tanimori
+ *
+ */
+@Component
+@ConfigurationProperties(prefix = "environments")
+@Data
 public class EnvironmentsConfiguration {
 	
 	/** CORSを許可するオリジンのURL */
