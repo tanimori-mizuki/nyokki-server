797cbc9618224ce4dff6239c6422a327147f4935
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jp  git config --list git config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-11-11 13:47:05 +0900

    ファイルの統一

--- src/main/java/com/example/common/EnvironmentsConfiguration.java
+++ src/main/java/com/example/common/EnvironmentsConfiguration.java
@@ -1,4 +1,4 @@
-package com.example9.common;
+package com.example.common;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
