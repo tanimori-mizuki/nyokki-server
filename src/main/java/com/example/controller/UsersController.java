0e08e59abb040b95a346b3b296f54be5ed84be96
fuka0726 <fuuka.pyop@gmail.com>
2020-11-11 16:12:05 +0900

    コントローラー修正

--- src/main/java/com/example/controller/UsersController.java
+++ src/main/java/com/example/controller/UsersController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.Users;
 import com.example.service.UsersService;
@@ -16,17 +18,18 @@ import com.example.service.UsersService;
  * @author fuka
  *
  */
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UsersController {
 
 	@Autowired
 	private UsersService usersService;
 
-	@RequestMapping("")
-	public List<Users> showUsers(Model model) {
+	@PostMapping("")
+	public List<Users> showUsers() {
 		List<Users> usersList = usersService.getAllUsers();
 		// model.addAttribute("usersList", usersList);
+		System.out.println(usersList);
 		return usersList;
 	}
 
