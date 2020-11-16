6dbfece014053007b47c87a6d8345f239471e0e6
fuka0726 <fuuka.pyop@gmail.com>
2020-11-16 15:59:21 +0900

    ResponseObjectリスト系データ取得成功

--- src/main/java/com/example/service/ShowTopPageService.java
+++ src/main/java/com/example/service/ShowTopPageService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Following;
 import com.example.domain.Todo;
 import com.example.domain.User;
 import com.example.dto.ResponseObject;
+import com.example.mapper.FollowingMapper;
 import com.example.mapper.TodoMapper;
 import com.example.mapper.UserMapper;
 
@@ -19,29 +21,37 @@ public class ShowTopPageService {
 	
 	@Autowired
 	private UserMapper userMapper;
-	
 	@Autowired
 	private TodoMapper todoMapper;
+	@Autowired
+	private FollowingMapper followingMapper;
 
 	public ResponseObject showTopPage() {
 		List<User> userList = userMapper.findAll();
 		List<Todo> todoList = todoMapper.findAll();
+		List<Following> followingList = followingMapper.findAll();
 		
 		ResponseObject responseObject = new ResponseObject();
 		responseObject.setUserList(userList);
-		
-		
 		responseObject.setTodoList(todoList);
+		responseObject.setFollowingList(followingList);
 		
 		System.out.println("ユーザーリスト" + userList);
 		for (int i = 0; i < userList.size(); i++) {
 			System.out.println(userList.get(i));
+			System.out.println(userList.get(i).getId());
+			System.out.println(userList.get(i).getName());
 		}
-		
 		System.out.println("todoリスト" + todoList);
 		for (int i = 0; i < todoList.size(); i++) {
 			System.out.println(todoList.get(i).getId());
 			System.out.println(todoList.get(i).getTask());
+		}
+		System.out.println("ユーザーリスト" + followingList);
+		for (int i = 0; i < followingList.size(); i++) {
+			System.out.println(followingList.get(i));
+			System.out.println(followingList.get(i).getId());
+			System.out.println(followingList.get(i).getFollowFlag());
 		}
 		
 		return responseObject;
