b5be854e13ffbb5db5878fbd846efe70622f53c9
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-11-20 16:03:45 +0900

    パスとコメントの追記を行いました。

--- src/main/java/com/example/controller/RegisterUserController.java
+++ src/main/java/com/example/controller/RegisterUserController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,12 @@ import com.example.domain.User;
 import com.example.form.RegisterUserForm;
 import com.example.service.RegisterUserService;
 
+/**
+ * ログインしたGoogleアカウントをusersテーブルにインサートするコントローラ.
+ * 
+ * @author mizuki.tanimori
+ *
+ */
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
@@ -19,11 +26,12 @@ public class RegisterUserController {
 	private RegisterUserService registerUserService;
 	
 	/**
-	 * ログインしたGoogleアカウントをusersテーブルにインサートするサービス.
+	 * usersテーブルにインサートするメソッド.
 	 * 
 	 * @param form
 	 * @return ログインしたユーザ情報
 	 */
+	@PostMapping("/register")
 	public User registerUser(@RequestBody RegisterUserForm form) {
 		return registerUserService.insertGoogleAccount(form);
 	}
