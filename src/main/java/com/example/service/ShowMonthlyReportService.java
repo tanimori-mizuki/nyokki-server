130366feed1fdb4f0bb5a82cb7a08a23e06b2c8d
fuka0726 <fuuka.pyop@gmail.com>
2021-01-26 16:01:56 +0900

    カレンダーの月報の目標を表示するように修正

--- src/main/java/com/example/service/ShowMonthlyReportService.java
+++ src/main/java/com/example/service/ShowMonthlyReportService.java
@@ -1,14 +1,17 @@
 package com.example.service;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.MonthlyReport;
+import com.example.domain.Objective;
 import com.example.dto.ResponceMonthlyReportObject;
 import com.example.mapper.MonthlyReportMapper;
+import com.example.mapper.ObjectiveMapper;
 
 /**
  * 月報ページを表示するためのサービス
@@ -22,12 +25,30 @@ public class ShowMonthlyReportService {
 	@Autowired
 	private MonthlyReportMapper monthlyReportMapper;
 	
+	@Autowired
+	private ObjectiveMapper objectiveMapper;
+	
 	public ResponceMonthlyReportObject showPastMonthlyReport(Integer userId, Integer year, Integer month) {
+		
+		try {
+			
 		ResponceMonthlyReportObject responceMonthlyReportObject = new ResponceMonthlyReportObject();
 		
 		MonthlyReport monthlyReport = monthlyReportMapper.findByDateAndUserId(userId, year, month);
+		Objective thisMonthObjective = objectiveMapper.selectByPrimaryKey(monthlyReport.getThisMonthObjectiveId());
+		Objective nextMonthObjective = objectiveMapper.selectByPrimaryKey(monthlyReport.getNextMonthObjectiveId());
 		responceMonthlyReportObject.setMonthlyReport(monthlyReport);
+		responceMonthlyReportObject.setThisMonthObjective(thisMonthObjective);
+		responceMonthlyReportObject.setNextMonthObjective(nextMonthObjective);
 		return responceMonthlyReportObject;
+		
+		}catch (NullPointerException e) {
+			System.err.print(e);
+			return null;
+		}
 	}
+	
+
+	
 	
 }
